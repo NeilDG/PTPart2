893340d0b400d557a4043be8dd52c0c554c66348
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-05-15 18:50:29 +0800

    Setup skybox.Setup of project.

---
+++ Assets/Scripts/UI/Views/CharacterSelectionView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectionView : View {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OnClickedWarrior(){
+		ViewHandler.Instance.Show (ViewNames.CHARACTER_PREVIEW_PANEL_STRING);
+		this.Hide ();
+	}
+
+	public void OnClickedRouge(){
+		ViewHandler.Instance.Show (ViewNames.CHARACTER_PREVIEW_PANEL_STRING);
+		this.Hide ();
+	}
+
+	public void OnClickedMage(){
+		ViewHandler.Instance.Show (ViewNames.CHARACTER_PREVIEW_PANEL_STRING);
+		this.Hide ();
+	}
+
+	public void OnClickedBack(){
+		this.Hide ();
+	}
+}
